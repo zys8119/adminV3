ab64114993f7503b574ba3567c97ec78741e2172
zys8119 <770959294@qq.com>
2021-05-17 15:30:21 +0800

    [add] 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="App">
+        asdasdaadasd
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped lang="less">
+.App{
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import {UserConfig} from 'vite'
+import pluginVue from '@vitejs/plugin-vue'
+export default <UserConfig>{
+    plugins: [
+        pluginVue()
+    ]
+}
