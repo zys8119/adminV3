8be1081618b8531c669232d45205a1ee54bf967a
zys8119 <770959294@qq.com>
2021-05-17 18:11:34 +0800

    [add] 初始化项目

--- src/store/airforce.ts
+++ src/store/airforce.ts
@@ -4,14 +4,55 @@ import _ from "lodash"
 export default <Module<any,any>>{
     state:{
         a:11,
-        input(ModuleName, keyPathName,value = {}){
-
-            store.commit("airforceSet",_.merge({},value))
+        bb:{a:"AAAA"},
+        cc:{},
+        dd:[1,2,3],
+        ee:{
+            a:1,
+            b:2,
+            c:[6,3,8],
+            d:{
+                a:2,
+                b:"asda"
+            }
+        },
+        input(ModuleName, keyPathName,value,replace){
+            let goods = _.set({},keyPathName,value);
+            let isMerge = true;
+            if(undefined === value){
+                goods = keyPathName;
+                isMerge = false;
+            }
+            store.commit("airforceSet",{
+                goods,
+                ModuleName,
+                isMerge,
+                value,
+                keyPathName,
+                replace
+            })
         }
     },
     mutations:{
-        airforceSet(state, payload){
-            console.log(state,payload,333)
+        airforceSet(state, {goods,ModuleName,isMerge,value,keyPathName,replace}){
+            switch (Object.prototype.toString.call(goods)){
+                case "[object Object]":
+                    if(isMerge){
+                        let Obj = _.merge((state[ModuleName] || {}),goods)
+                        state[ModuleName] = Obj;
+                        if(replace){
+                            state[ModuleName] = _.set(Obj,keyPathName,value);
+                        }
+                    }else {
+                        state[ModuleName] = goods;
+                    }
+                    break;
+                default:
+                    state[ModuleName] = goods;
+                    break;
+            }
+            // console.log(goods)
+
         }
     },
     actions:{
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -8,9 +8,16 @@
 export default {
     name: "Home",
     mounted() {
-        console.log(this.$store,111)
-        console.log(this.airforce)
+        // console.log(this.$store,111)
+        // console.log(this.airforce)
         this.airforce.input("aa","a.b.c",{})
+        this.airforce.input("bb",{})
+        this.airforce.input("bb","asdad")
+        this.airforce.input("bb","asdad",0)
+        // this.airforce.input("bb","asdad",false)
+        // this.airforce.input("cc","asdad",null)
+        this.airforce.input("dd","[1]",[454])
+        this.airforce.input("ee","c",[454])
         console.log(this.airforce)
     }
 }
