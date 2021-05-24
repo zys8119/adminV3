dd272333536a11987cce1c28522c55b54be29417
zys8119 <770959294@qq.com>
2021-05-24 15:19:04 +0800

    [add] api注入

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,10 @@
 import { AxiosPromise } from "axios"
 import { ComponentInternalInstance } from "vue"
-export type apiOpt <T = any> = (this:ComponentInternalInstance)=>AxiosPromise<T> | T;
+export type apiOpt <T = any> = (this:ComponentInternalInstance,...args:any)=>AxiosPromise<T> | T;
 export default <Partial<apiOpt>>{
+    a:{
 
+    },
+    bb(){
+    }
 }
