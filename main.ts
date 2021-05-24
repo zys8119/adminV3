cb695d2aec8f0f2e740a7b5c7e40f62df2dc8655
zys8119 <770959294@qq.com>
2021-05-24 14:12:33 +0800

    [add] ui注入

--- main.ts
+++ main.ts
@@ -1,15 +1,17 @@
 import {createApp} from "vue"
-import store from "./src/store"
-import airforcePlug from "./src/store/plug/airforcePlug"
-import alertPlug from "./src/store/plug/alertPlug"
 import router from "./src/router"
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from "./src/App.vue"
+import store from "./src/store"
+import airforcePlug from "./src/store/plug/airforcePlug"
+import alertPlug from "./src/store/plug/alertPlug"
+import uiPlug from "./src/ui"
 createApp(<any>App)
     .use(ElementPlus)
     .use(router)
     .use(store)
     .use(airforcePlug)
     .use(alertPlug)
+    .use(uiPlug)
     .mount("#app")
---
+++ src/ui/index.ts
@@ -0,0 +1,7 @@
+import { Plugin } from "vue"
+const plug:Plugin = {
+    install(vue){
+        
+    }
+}
+export default plug
