7f33e7c0272f515a696fa86180d1558475fb3f47
zys8119 <770959294@qq.com>
2021-06-03 17:21:07 +0800

    [add] ui注入-Transfer.vue

--- src/ui/package/Transfer/TransferTree.vue
+++ src/ui/package/Transfer/TransferTree.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="TransferTree">
+        <el-input @change="searchChange" v-model="search" v-if="showSearch" :placeholder="searchPlaceholder"></el-input>
+        <div class="TransferTreeNode" v-if="showCheckbox">
+            <div>
+                <el-checkbox v-model="checkboxAll" @change="checkboxChangeAll($event)"></el-checkbox>
+                <span>全选</span>
+            </div>
+        </div>
         <template v-for="(item,key) in currentOptions" :key="key">
             <div class="TransferTreeNode" v-if="item.is_open" @click="nodeClick(item,'nodeClick')">
-                <div :style="{marginLeft:`${(item.level-1)*15}px`}">
-                    <el-checkbox v-model="item.checkbox" @change="checkboxChange($event, item)"></el-checkbox>
-                    <span>{{item.data[fieldName]}}</span>
+                <div :style="{marginLeft:search ? null : `${(item.level-1)*15}px`}">
+                    <el-checkbox v-model="item.checkbox" @change="checkboxChange($event, item)"  v-if="showCheckbox"></el-checkbox>
+                    <slot v-bind:="item">
+                        <span>{{item.data[fieldName]}}</span>
+                    </slot>
                 </div>
             </div>
         </template>
@@ -13,17 +22,21 @@
 
 <script lang="ts">
 
-import fa from "element-plus/packages/locale/lang/fa";
-
 export default {
     name: "TransferTree",
     data(){
         return {
+            checkboxAll:false,
+            currentOptionsMap:{},
+            search:null,
         }
     },
     props:{
         fieldName:{type:String,default:"name"},
         childrenFieldName:{type:String,default:"children"},
+        showSearch:{type:Boolean,default:true},
+        showCheckbox:{type:Boolean,default:true},
+        searchPlaceholder:{type:String,default:"请输入关键字"},
         options:{
             type:Array,
             default:()=>[
@@ -50,11 +63,17 @@ export default {
         }
     },
     computed:{
+        copyOptions:{
+            get(){
+                return this.options;
+            },
+            set(value){
+                return value;
+            }
+        },
         currentOptions:{
             get(){
-                let result = [];
-                this.currentOptionsInit((this.options || []),result,{});
-                return result;
+                return this.getCurrentOptions();
             },
             set(val){
                 return val;
@@ -62,6 +81,12 @@ export default {
         }
     },
     methods:{
+        getCurrentOptions(){
+            let result = [];
+            this.currentOptionsMap = {};
+            this.currentOptionsInit((JSON.parse(JSON.stringify(this.copyOptions || []))),result,{});
+            return result;
+        },
         currentOptionsInit(options,result,extra:any = {}){
             options.forEach((it,key)=>{
                 const node = {
@@ -71,6 +96,7 @@ export default {
                     deep:extra.deep ? (<Array<number>>extra.deep).concat([key]) : [key],
                     checkbox:false,
                 }
+                this.currentOptionsMap[JSON.stringify(node.deep)] = node;
                 result.push(node);
                 if(it[this.childrenFieldName] && Object.prototype.toString.call(it[this.childrenFieldName]) === "[object Array]"){
                     this.currentOptionsInit(it[this.childrenFieldName], result,{
@@ -81,23 +107,55 @@ export default {
                 }
             })
         },
-        nodeClick(item, type){
+        nodeClick(item, type, extra:any = {}){
             let parentNode = null;
             const itemChildren = this.currentOptions.filter(e=>{
                 if(JSON.stringify(item.deep.slice(0,item.deep.length -1)) === JSON.stringify(e.deep)){
                     parentNode = e;
                 }
-                return e !== item && item.deep.length+1 === e.deep.length && JSON.stringify((<number[]>e.deep).slice(0,item.deep.length)) === JSON.stringify(item.deep);
+                let bool = item.deep.length+1 === e.deep.length;
+                if(type === "checkbox"){
+                    bool = true;
+                }
+                return e !== item && bool && JSON.stringify((<number[]>e.deep).slice(0,item.deep.length)) === JSON.stringify(item.deep);
             });
             itemChildren.forEach(it=>{
-                it.is_open = !it.is_open;
+                if(type === "checkbox"){
+                    it.checkbox = extra.val;
+                }else {
+                    it.is_open = !it.is_open;
+                }
             })
             this.$forceUpdate();
-            this.$emit(type,item, parentNode);
+            this.$emit(type,item, parentNode,extra);
         },
         checkboxChange(val, item){
-            console.log(item)
-            // this.nodeClick(val, item)
+            this.nodeClick(item, "checkbox", {val})
+            this.getNodeDeep(item)
+        },
+        checkboxChangeAll(val){
+            this.currentOptions.forEach(it=>{
+                it.checkbox = val;
+            });
+            this.$forceUpdate();
+            this.$emit("checkbox",{val});
+        },
+        getSelection(){
+            return this.currentOptions.filter(e=>e.checkbox)
+        },
+        getNodeDeep(node){
+            return node.deep.map((e,k)=>{
+                return this.currentOptionsMap[JSON.stringify(node.deep.slice(0,k+1))];
+            })
+        },
+        searchChange(val){
+            if(val){
+                this.currentOptions.forEach(it=>{
+                    it.is_open = it.data[this.fieldName].indexOf(val) > -1;
+                });
+            }else {
+                this.currentOptions = this.getCurrentOptions();
+            }
             this.$forceUpdate();
         }
     }
