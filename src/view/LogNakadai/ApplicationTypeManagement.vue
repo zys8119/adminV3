4f84e1e956f8aa52cafe7f3d533634dec14afab2
zys8119 <770959294@qq.com>
2021-08-26 15:30:02 +0800

    [add] 日志监控系统-应用类型

--- src/view/LogNakadai/ApplicationTypeManagement.vue
+++ src/view/LogNakadai/ApplicationTypeManagement.vue
@@ -2,11 +2,12 @@
     <div class="ApplicationTypeManagement">
         <filter-content v-model="params.search" :btns="[
             {name:'新增应用类型', emit:'add'}
-        ]" @add="add"></filter-content>
+        ]" @add="add" @search="search" @reset="reset"></filter-content>
         <content-table
             ref="table"
             :apiPath="api.LogNakadai.ApplicationType.list"
             :columns="columns"
+            :params="params"
         ></content-table>
     </div>
 </template>
@@ -21,7 +22,7 @@ export default {
             params:{},
             columns:<Array<columns>>[
                 {label:"应用名称", prop:"name"},
-                {label:"应用id", prop:"name"},
+                {label:"应用id", prop:"id"},
                 {label:"操作", type:"operate", btns:[
                     {name:"编辑", type:"text", className:"primary"},
                     {name:"删除", type:"text", className:"default"},
@@ -37,7 +38,9 @@ export default {
             this.reset()
         },
         reset(){
-            this.params = {};
+            this.params = {
+                search:null
+            };
             this.search();
         },
         search(){
